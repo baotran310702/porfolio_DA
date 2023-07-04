19f12f90e5433710f2957af842d1d2cbe89e142b
baotran310702 <tranbao1414@gmail.com>
2023-07-04 07:07:36 +0700

    update cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
 
               <div className="flex items-center self-start mt-2">
                 <Link
-                  href="/BaoTranCV.pdf"
+                  href="/BaoTran_CV.pdf"
                   className={
                     "flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
                   }
